35b67166ae8f9771e834973449f3f8e2c8233892
Ravi <ravi@example.com>
2025-03-23 17:04:25 -0300

    finished

--- index.php
+++ index.php
@@ -8,26 +8,90 @@ function createPerson()
 
     $person = [
         "name" => $faker->name(),
-        "emaill" => $faker->email(),
+        "email" => $faker->email(),
         "message" => $faker->text()
     ];
+
+    return $person;
 }
+
+function showPeople(int $quantity)
+{
+    for ($i = 1; $i <= $quantity; $i++) {
+
+        $person = createPerson();
+
+        $name = $person['name'];
+        $email = $person['email'];
+        $message = $person['message'];
+
+        echo "
+        <div class='card mt-3'>
+                    <div class='card-header'>
+                        <h3>
+                            $name
+                        </h3>
+                    </div>
+                    <div class='card-body'>
+                        <p>
+                            <span class='fw-bold'>
+                                Email:
+                            </span>
+                            $email
+                        </p>
+
+                        <div class='card-text'>
+                            <span class='fw-bold'>
+                            Mensagem: 
+                            </span>
+                            <div>
+                            $message
+                            </div>
+                        </div>
+                        <code class='mt-2'>
+                            <pre>".
+                            json_encode($person, JSON_PRETTY_PRINT)
+                            ."
+                            </pre>
+                        </code>
+                    </div>
+                </div>
+        ";
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
+
 <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Hello!</title>
     <style>
-        @import './src/public/assets/bootstrap.min.css';
+        @import './vendor/twbs/bootstrap/dist/css/bootstrap.min.css';
     </style>
 </head>
 
 <body>
 
-    <h1>Gerador de Pessoas</h1>
-    
+    <main class='p-3'>
+        <h1>Gerador de Pessoas</h1>
+
+        <p class='my-3'>Atualize a página para conhecer novas pessoas :)</p>
+
+        <?php
+
+            showPeople(10);
+
+        ?>
+
+
+    </main>
+
 </body>
+<script src='/vendor/components/jquery/jquery.min.js"></script>
+<script>
+</script>
 
 </html>
